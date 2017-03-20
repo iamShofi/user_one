e722f27407eea617ef84d07dbd92c0e7690817c3
user_one <user9x1@gmail.com>
2017-03-21 02:00:30 +0600

    Food Web

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'contact/contact_us'
+
   get 'home/index'
+  get 'index' => 'home#index'
 
   root 'home#index'
 
   get 'about' => 'about#about'
+
+  get 'contact' => 'contact#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
